27b117eccd24d9ae39748916f73fd363b3c581e8
Crystal Goodwin <sgcg03@mac.com>
2023-04-24 15:02:27 -0400

    Add image gallery section to html, css and js

--- js/script.js
+++ js/script.js
@@ -14,12 +14,31 @@ $(document).ready(function () {
       }
    });
 
-   //sample image popup
+   //single image popup
    $('.image-popup-fit-width').magnificPopup({
       type: 'image',
       closeOnContentClick: true,
       image: {
          verticalFit: false
+      }
+   });
+
+   //gallery image popup
+   $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+         enabled: true,
+         navigateByImgClick: true,
+         preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+         titleSrc: function (item) {
+            return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+         }
       }
    });
 
